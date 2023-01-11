0f18efe316a0b0342ec88f4cefc0735557882cd9
Andrés <c.andresricardo@gmail.com>
2023-01-11 00:02:46 -0300

    ✨ add files

---
+++ contracts/interfaces/Withdrawable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/token/ERC20/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/access/Ownable.sol";
+
+contract Withdrawable {
+    
+}
---
+++ contracts/utils/FlashloanRecieverBase.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "../interfaces/ILendingPoolAddressesProvider.sol";
+import "../interfaces/IFlashLoanReceiver.sol";
+
+abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
+
+}
