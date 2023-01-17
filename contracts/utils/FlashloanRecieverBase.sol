bb066e3d5a5d823e838c4665581e139b66cb3731
Andrés <c.andresricardo@gmail.com>
2023-01-17 16:37:57 -0300

    🚧 test flash loan

--- contracts/utils/FlashloanRecieverBase.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
-
-import "../interfaces/ILendingPoolAddressesProvider.sol";
-import "../interfaces/IFlashLoanReceiver.sol";
-
-abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
-
-}
